57c6a1ae6abee987eb70d21fdb62e011fdc2752c
Jeann Lucas <jeann.lucas@outlook.com>
2023-08-07 22:55:47 -0300

    Create Component

---
+++ src/componentes/Logo/index.js
@@ -0,0 +1,18 @@
+import './estilo.css'
+import logo from '../../imagens/logo.svg'
+
+function Logo() {
+    return (
+        <div className='logo'>
+            <img
+                src={logo}
+                alt='logo'
+                className='logo-img'>
+            </img>
+            <p><strong>BigDevZ</strong> | Books</p>
+        </div>
+    )
+}
+
+
+export default Logo
