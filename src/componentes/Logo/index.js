cb0b8ddfd75da6400e7784e99b760acb04c9b3a8
Jeann Lucas <jeann.lucas@outlook.com>
2023-08-15 18:05:12 -0300

    Styled Components

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,16 +1,25 @@
-import './estilo.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+    margin-left: 25px;
+`
+
+const LogoImage = styled.img`
+    margin-right: 10px;
+`
 
 function Logo() {
     return (
-        <div className='logo'>
-            <img
+        <LogoContainer>
+            <LogoImage
                 src={logo}
                 alt='logo'
-                className='logo-img'>
-            </img>
+            />
             <p><strong>BigDevZ</strong> | Books</p>
-        </div>
+        </LogoContainer>
     )
 }
 
